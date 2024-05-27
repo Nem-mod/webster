b0de79566c7f4ae9f6e12a3552d17dd1694fd33a
nechamdod <nechamdod@gmail.com>
2024-05-27 14:04:20 +0300

    feat: nextUI

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,3 +1,4 @@
+import { NextUIProvider } from '@nextui-org/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,8 +8,10 @@ import { store } from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<NextUIProvider>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</NextUIProvider>
 	</React.StrictMode>
 );
