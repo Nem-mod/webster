73f6d0c7c4eb13058dc8a8ec928fd33f7d20bad6
nechamdod <nechamdod@gmail.com>
2024-05-27 14:43:26 +0300

    feat: opacity input + fixes

--- app/src/components/CanvasEditBar/CanvasEditBar.tsx
+++ app/src/components/CanvasEditBar/CanvasEditBar.tsx
@@ -1,29 +1,55 @@
+import { Slider } from '@nextui-org/react';
 import { useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { moveElements, moveElementsOneStep, updateElement } from '../../redux/slices/canvasSlice/canvas-slice';
+
 export default function CanvasEditBar() {
 	const dispatch = useAppDispatch();
 	const selectedElements = useAppSelector((state) => state.canvas.data?.selected);
 	const [color, setColor] = useState<string>('#aabbcc');
-
+	const [colorTimeout, setColorTimeout] = useState<number>();
+	const [opacity, setOpacity] = useState<number | number[]>(1);
 	if (!selectedElements?.length) return <></>;
 
 	const elementsTypes = selectedElements?.map((e) => e.type);
-	const handleChangeFill = (color: string) => {
-		setColor(color);
+
+	const handleChangeColor = (value: string) => {
+		if (colorTimeout) {
+			clearTimeout(colorTimeout);
+		}
+
+		const timeoutId = setTimeout(() => {
+			selectedElements.forEach((e) => {
+				dispatch(
+					updateElement({
+						index: e.index,
+						element: {
+							fill: value,
+						},
+					})
+				);
+			});
+		}, 500);
+		setColorTimeout(timeoutId);
+	};
+
+	const handleChangeOpacity = () => {
 		selectedElements.forEach((e) => {
 			dispatch(
 				updateElement({
 					index: e.index,
 					element: {
-						fill: color,
+						opacity: opacity as number,
 					},
 				})
 			);
 		});
 	};
 
+	const handleMoveMax = (to: boolean) => {
+		dispatch(moveElements({ to }));
+	};
 	const handleMoveOneStep = (to: boolean) => {
 		dispatch(moveElementsOneStep({ to }));
 	};
@@ -31,10 +57,10 @@ export default function CanvasEditBar() {
 	return (
 		<div>
 			<p>{JSON.stringify(elementsTypes)}</p>
-			<HexColorPicker color={color} onChange={handleChangeFill} />
+			<HexColorPicker color={color} onChange={handleChangeColor} />
 			<button
 				onClick={() => {
-					dispatch(moveElements({ to: true }));
+					handleMoveMax(true);
 				}}
 			>
 				Move Top Max
@@ -42,7 +68,7 @@ export default function CanvasEditBar() {
 			<br />
 			<button
 				onClick={() => {
-					dispatch(moveElements({ to: false }));
+					handleMoveMax(false);
 				}}
 			>
 				Move Bot Max
@@ -63,6 +89,16 @@ export default function CanvasEditBar() {
 			>
 				Move bot
 			</button>
+			<Slider
+				label='Opacity'
+				step={0.01}
+				maxValue={1}
+				minValue={0}
+				value={opacity}
+				onChange={setOpacity}
+				onChangeEnd={handleChangeOpacity}
+				className='max-w-sm'
+			/>
 		</div>
 	);
 }
