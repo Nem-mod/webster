b842880a66d1ed0e82ac287d351bc72683a81419
nechamdod <nechamdod@gmail.com>
2024-05-27 14:04:34 +0300

    feat: nextUI

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,12 +4,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    [nextui()]
-  ],
+  darkMode: "class",
+  plugins: [nextui()]
 }
