7d4ba0f0911767633e2c0eb474099aa97a21d9fb
nechamdod <nechamdod@gmail.com>
2024-06-05 17:20:38 +0300

    WIP: image filters

--- app/src/components/CanvasEditBar/ImageFilter/ImageEditFilter.tsx
+++ app/src/components/CanvasEditBar/ImageFilter/ImageEditFilter.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectItem } from '@nextui-org/react';
+import { Select, SelectItem, Slider } from '@nextui-org/react';
 import { ICanvasElement } from '../../../services/canvas/canvas.types';
 import Konva from 'konva';
 import { useEffect, useState } from 'react';
@@ -9,43 +9,117 @@ interface IProps {
 	elements: ICanvasElement[];
 }
 
-const filters = Object.entries(Konva.Filters);
+const filterHasAttrs = {
+	Blur: {
+		key: 'blurRadius',
+		value: 0,
+		maxValue: 100,
+		minValue: 0,
+	},
+	Brighten: {
+		key: 'brightness',
+		value: 0,
+		maxValue: 1,
+		minValue: -1,
+		step: 0.02,
+	},
+	Contrast: {
+		key: 'contrast',
+		value: 0,
+		maxValue: 100,
+		minValue: -100,
+		step: 1,
+	},
+	Enhance: {
+		key: 'enhance',
+		value: 0,
+		maxValue: 1,
+		minValue: -1,
+		step: 0.01,
+	},
+	Noise: {
+		key: 'noise',
+		value: 0,
+		maxValue: 4,
+		minValue: 0,
+		step: 0.1,
+	}
+};
+
+interface IFilterObject {
+	name: string;
+	attrs: {
+		key: string;
+		value: number;
+		maxValue: number;
+		minValue: number;
+		step: number;
+	} | null;
+}
+
+const filters = Object.keys(Konva.Filters).filter(e => {
+	return !["HSL", "HSV", "RGB", "RGBA", "Mask", "Posterize", "Threshold"].includes(e)
+}).map((filterName) => {
+	// if (filterName === 'Emboss') return;
+	const filterObject: IFilterObject = {
+		name: filterName,
+		attrs: filterHasAttrs[filterName] || null,
+	};
+
+	return filterObject;
+});
+
+function FilterValueSlider({
+	data,
+	onChange,
+}: {
+	data: IFilterObject;
+	onChange: (args?: object) => void;
+}) {
+	const [value, setValue] = useState<number | number[]>(data.attrs?.value || 0);
+	useEffect(() => {
+		if (!data.attrs) return;
+
+		onChange({ [data.attrs.key]: value });
+	}, [value]);
+	return (
+		<Slider
+			step={data.attrs?.step || 0.5}
+			maxValue={data.attrs?.maxValue || 100}
+			minValue={data.attrs?.minValue || 1}
+			size={'sm'}
+			value={value}
+			onChange={setValue}
+			// onChangeEnd={handleChangeOpacity}
+		/>
+	);
+}
 
 export default function ImageEditFilter({ elements }: IProps) {
 	const dispatch = useAppDispatch();
 	const elementsFilters = elements.reduce((flattenedArray, curr) => {
-		console.log('curr', curr);
 		if (!curr.filters) return flattenedArray;
-
 		return flattenedArray.concat(curr?.filters);
 	}, []);
-	
+
 	const [value, setValue] = useState<Selection>(new Set(elementsFilters));
-	useEffect(() => {
+
+	const handleChangeFilter = (args?: object) => {
 		const filters = [...value];
-		console.log('filters', filters);
 
 		elements.forEach((element) => {
 			dispatch(
 				updateElement({
 					index: element.index,
-					element: { ...element, filters: filters },
+					element: { ...element, filters: filters, ...args },
 				})
 			);
 		});
-	}, [value]);
+	};
 
-	// const handleChangeFilter = () => {
-	// 	elements.forEach((element) => {
-	// 		dispatch(updateElement({ ...element ,
-	// 			filters: [...value].map(filter =>
-	// 				{
-	// 					console.log('filter',filter)
-	// 				}
-	// 			)
-	// 		}));
-	// 	});
-	// };
+	useEffect(() => {
+		handleChangeFilter();
+	}, [value]);
 
 	return (
 		<Select
@@ -56,7 +130,12 @@ export default function ImageEditFilter({ elements }: IProps) {
 			className='max-w-xs'
 		>
 			{filters.map((filter) => (
-				<SelectItem key={filter[0]}>{filter[0]}</SelectItem>
+				<SelectItem key={filter.name}>
+					<span>{filter.name}</span>
+					{filter.attrs && (
+						<FilterValueSlider data={filter} onChange={handleChangeFilter} />
+					)}
+				</SelectItem>
 			))}
 		</Select>
 	);
