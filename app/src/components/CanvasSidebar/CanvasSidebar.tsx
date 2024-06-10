39ce54a30588031174890b2ff217cec65e0dca75
NorthWind08 <95715602+NorthWind08@users.noreply.github.com>
2024-06-10 14:38:00 +0300

    FEAT: cool close button for sidebar

--- app/src/components/CanvasSidebar/CanvasSidebar.tsx
+++ app/src/components/CanvasSidebar/CanvasSidebar.tsx
@@ -2,8 +2,6 @@ import {Tabs, Tab, ScrollShadow, Button} from '@nextui-org/react';
 import { useState } from 'react';
 import CanvasMenu from '../CanvasMenuGroup/CanvasMenu/CanvasMenu.tsx';
 import HistoryList from '../HistoryList/HistoryList.tsx';
-import { Image } from "@nextui-org/react"
-import SmallButton from "./SmallButton.tsx";
 
 
 
@@ -15,11 +13,30 @@ export const CanvasSidebar = () => {
 	};
 
 	return (
-		<div className={'flex flex-row justify-center h-[calc(100vh_-_64px)]'}>
+		<div className={'relative flex flex-row justify-center h-[calc(100vh_-_64px)]'}>
 			{selected !== 'null' &&
-				<div className={'absolute left-0 ml-[4.7rem]'}>
-					<SmallButton onClick={unselectTab}/>
-				</div>}
+					<div
+						className='absolute -right-3 top-[25rem] rounded-r rounded-l-none '
+						onClick={() => setSelected('null')}
+					>
+						{/* <div className='bg-red-900 w-2 h-5'>
+					
+						</div>
+						<div className='bg-red-900'>
+							button
+						</div>
+						<div className='bg-red-900 w-2 h-5'>
+							
+						</div> */}
+						<svg xmlns="http://www.w3.org/2000/svg" width={13} height={96} viewBox='0 0 13 96'
+							className='fill-secondary/20 cursor-pointer'
+						>
+							<path d="M0,0 h1 c0,20,12,12,12,32 v32 c0,20,-12,12,-12,32 H0 z"></path>
+							<path d="M0.5,0 c0,20,12,12,12,32 v32 c0,20,-12,12,-12,32"></path>
+							<path stroke="currentColor" stroke-linecap="round" stroke-width="1.25" d="M 3.5,47 l 5,-5 M 3.5,47 l 5,5"></path>
+						</svg>
+					</div>
+				}
 			<Tabs
 				selectedKey={selected}
 				onSelectionChange={setSelected}
@@ -32,17 +49,21 @@ export const CanvasSidebar = () => {
 					panel: 'flex  justify-center m-2 overflow-auto max-h-full',
 				}}
 			>
+				
 				<Tab key={'null'} title={'Null'} className={'hidden'}></Tab>
 				<Tab key={'tools'} title={'Tools'}>
-					<div className={'flex flex-col'}>
-						{/*<Button isIconOnly className={'bg-secondary/70'} size={'sm'}>*/}
-						{/*	X*/}
-						{/*</Button>*/}
-						{/*<span className={'ml-auto  rounded-xl rounded-r-none rounded-t-none aspect-square text-center'} onClick={unselectTab}>*/}
-						{/*	X*/}
-						{/*</span>*/}
-						<CanvasMenu />
-					</div>
+					{/* <SidebarSelectedWrapper setSelected={setSelected}> */}
+						<div className={'flex flex-col'}>
+							{/*<Button isIconOnly className={'bg-secondary/70'} size={'sm'}>*/}
+							{/*	X*/}
+							{/*</Button>*/}
+							{/*<span className={'ml-auto  rounded-xl rounded-r-none rounded-t-none aspect-square text-center'} onClick={unselectTab}>*/}
+							{/*	X*/}
+							{/*</span>*/}
+							<CanvasMenu />
+							
+						</div>
+					{/* </SidebarSelectedWrapper> */}
 				</Tab>
 				<Tab key={'images'} title={'Images'}>
 
