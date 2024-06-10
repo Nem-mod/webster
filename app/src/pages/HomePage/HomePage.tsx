2d48c85eb11c59196aad8a749494f271b4ea9ca4
Miconbeko <stepanov.m2004@gmail.com>
2024-06-10 18:08:05 +0300

    feat: canvas cards scale fix on home page

--- app/src/pages/HomePage/HomePage.tsx
+++ app/src/pages/HomePage/HomePage.tsx
@@ -23,49 +23,44 @@ export default function HomePage() {
 
 	return (
 		<div className={'flex flex-col h-screen'}>
-			<CustomNavBar />
-			<div
-				className={`flex justify-center grow w-screen bg-gradient-to-bl from-accent/20 to-light/20`}
-			>
+			<CustomNavBar/>
+			<div className={`flex justify-center grow w-screen bg-gradient-to-bl from-accent/20 to-light/20 overflow-x-hidden`}>
 				<div className='pt-20 max-w-6xl'>
-					<div className={'flex gap-10'}>
-						<Input
-							classNames={{
-								base: 'w-full h-10',
-								mainWrapper: 'h-full',
-								input: 'text-large',
-								inputWrapper:
-									'h-full font-normal text-default-500 bg-accent/50 dark:bg-accent/20',
-							}}
-							placeholder='Type to search...'
-							size='lg'
-							startContent={<SearchIcon size={18} />}
-							type='search'
-							onChange={(e) => handleSearch(e.target.value)}
-						/>
-						<Button
-							className={'text-xl bg-primary/90'}
-							size={'lg'}
-							color={'primary'}
-							onPress={onOpen}
-						>
-							Create new
-						</Button>
-						<CreateEditCanvasModal
-							isOpen={isOpen}
-							onOpenChange={onOpenChange}
-						/>
-					</div>
-					<div className={'flex mt-10 gap-10 flex-wrap justify-center'}>
-						{canvases &&
-							canvases.map((canvas) => (
-								<CanvasCard
-									key={canvas._id}
-									canvas={canvas}
-									to={`/workspace/${canvas._id}`}
-								/>
-							))}
-					</div>
+						<div className={'flex gap-10'}>
+							<Input
+								classNames={{
+									base: 'w-full h-10',
+									mainWrapper: 'h-full',
+									input: 'text-large',
+									inputWrapper:
+										'h-full font-normal text-default-500 bg-accent/50 dark:bg-accent/20'
+								}}
+								placeholder='Type to search...'
+								size='lg'
+								startContent={<SearchIcon size={18} />}
+								type='search'
+                onChange={(e) => handleSearch(e.target.value)}
+							/>
+							<Button
+								className={'text-xl bg-primary/90'}
+								size={'lg'}
+								color={'primary'}
+								onPress={onOpen}
+							>
+								Create new
+							</Button>
+							<CreateEditCanvasModal isOpen={isOpen} onOpenChange={onOpenChange}/>
+						</div>
+						<div className={'flex mt-10 gap-10 flex-wrap justify-center'}>
+							{canvases &&
+								canvases.map((canvas) => (
+									<CanvasCard
+										key={canvas._id}
+										canvas={canvas}
+										to={`/workspace/${canvas._id}`}
+									/>
+								))}
+						</div>
 				</div>
 			</div>
 		</div>
